94204ae436c8f44d1f8467f218511bea6ffdb03e
Caberu <lecaberudo@gmail.com>
2023-04-22 19:37:14 +0200

    BloodFX fix

---
+++ DestroyAfter.gd
@@ -0,0 +1,11 @@
+extends Particles2D
+
+export var timer = 1.0
+var destroyNow = 0.0
+
+func _ready():
+	destroyNow = OS.get_ticks_msec() + timer*1000 #en millisecond
+
+func _process(delta):
+	if (OS.get_ticks_msec() >= destroyNow):
+		self.queue_free()
