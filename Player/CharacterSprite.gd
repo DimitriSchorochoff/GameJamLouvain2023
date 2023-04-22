21c9a9d2a3a6df46fada30f7610d4cbb12e33666
DimitriSchorochoff <62765707+DimitriSchorochoff@users.noreply.github.com>
2023-04-22 15:28:40 +0200

    Merge

--- Player/CharacterSprite.gd
+++ Player/CharacterSprite.gd
@@ -1,14 +1,10 @@
 extends Sprite
 
 
-export onready var kinematic = $PlayerKineteic 
+#export onready var kinematic = $PlayerKineteic 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	flip_h = kinematic.face_h == 1
+#func _process(delta):
+#	flip_h = kinematic.face_h == 1
