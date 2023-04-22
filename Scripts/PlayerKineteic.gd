89b82c09580160f475db3b1943461de7120b3aa0
Caberu <lecaberudo@gmail.com>
2023-04-22 16:56:18 +0200

    Hauteur de saut dynamique et poussiere

--- Scripts/PlayerKineteic.gd
+++ Scripts/PlayerKineteic.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 
 const UP_DIRECTION := Vector2.UP
 
-export var speed = 220
+export var speed = 220.0
+export var acc = 1500.0
+export var fric = 1500.0
 export var jump_strenght := 600.0
 export var gravity := 1700.0
 
@@ -14,6 +16,7 @@ export var face_h := 1.0
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
+onready var particleRun = $Particles2D
 
 func _process(delta):
 	sprite.flip_h = face_h == 1
@@ -27,24 +30,31 @@ func _physics_process(delta: float) -> void:
 	if _horizontal_direction != 0.0:
 		face_h = _horizontal_direction/abs(_horizontal_direction)
 
-	_velocity.x = _horizontal_direction * speed
+		_velocity.x = move_toward(_velocity.x,_horizontal_direction *speed,  acc * delta)
+		
+	else:
+		_velocity.x = move_toward(_velocity.x, 0,  fric * delta)
 	_velocity.y += gravity * delta
 
 	var is_falling := _velocity.y > 0.0 and not is_on_floor()
 	var is_jumping := Input.is_action_just_pressed("jump") and is_on_floor()
-	var is_jump_cancelled := Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var is_jump_cancelled := Input.is_action_just_released("jump") and _velocity.y < 0.0 and !is_on_floor()
 	var is_idling := is_zero_approx(_velocity.x) and is_on_floor()
 	var is_running := is_on_floor() and not is_zero_approx(_velocity.x)
 	
 	if is_idling:
 		animationPlayer.play("Idle")
+		particleRun.emitting = false
 	elif is_running:
 		animationPlayer.play("Run")
+		particleRun.emitting = true
 	
-	print(is_on_floor())
 	if is_jumping:
 		_jumps_made += 1
 		_velocity.y -= jump_strenght
+	
+	if is_jump_cancelled:
+		_velocity.y /= 3
 		
 	elif is_idling or is_running:
 		_jumps_made = 0
