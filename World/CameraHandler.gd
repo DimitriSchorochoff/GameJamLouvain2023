1fad5260ef76e42852eec6438179c140f491cb86
Caberu <lecaberudo@gmail.com>
2023-04-23 11:20:22 +0200

    camera

--- World/CameraHandler.gd
+++ World/CameraHandler.gd
@@ -4,7 +4,7 @@ const CENTER_Y = 190
 
 export var speed = 200.0
 var _velocity = Vector2.ZERO
-	
+onready var remoteTr = $RemoteTransform2D
 
 func loadGame():
 	GameManager.GAME_STARTING = true
@@ -13,8 +13,9 @@ func _process(delta: float) -> void:
 	if GameManager.GAME_STARTING:
 		position = position.move_toward(Vector2(0,0), delta * speed)
 		
-	if position.is_equal_approx(Vector2.ZERO):
+	if position.is_equal_approx(Vector2.ZERO) && GameManager.GAME_STARTED != true:
 		GameManager.GAME_STARTED = true
+		remoteTr.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
