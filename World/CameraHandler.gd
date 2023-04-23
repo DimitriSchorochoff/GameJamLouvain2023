6670cf8964cc5a9766e82a7aaabd93b31c5df491
DimitriSchorochoff <62765707+DimitriSchorochoff@users.noreply.github.com>
2023-04-23 11:01:33 +0200

    Push start menu

--- World/CameraHandler.gd
+++ World/CameraHandler.gd
@@ -2,11 +2,8 @@ extends Sprite
 
 const CENTER_Y = 190
 
-export var speed = 150.0
+export var speed = 200.0
 var _velocity = Vector2.ZERO
-
-func _ready():
-	loadGame()
 	
 
 func loadGame():
@@ -16,11 +13,11 @@ func _process(delta: float) -> void:
 	if GameManager.GAME_STARTING:
 		position = position.move_toward(Vector2(0,0), delta * speed)
 		
-	print(position)
 	if position.is_equal_approx(Vector2.ZERO):
-		print("Yooo")
 		GameManager.GAME_STARTED = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+func _on_Button_pressed():
+	print("Hoho")
+	loadGame()
